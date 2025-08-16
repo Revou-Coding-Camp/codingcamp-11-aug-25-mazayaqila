c211ec6d76299e8042ad12a2c747a20f85e2bf38
zaa <studzaa@gmail.com>
2025-08-16 19:08:53 +0700

    16/08/25 19.08

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ document.getElementById("messageForm").addEventListener("submit", function (e) {
   // Fokus kembali ke input nama
   document.getElementById("nama").focus();
 });
+
+//Toggle class active
+const navbarNav = document.querySelector(".navbar");
+
+//Hamburger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik luar sidebar buat ngilangin nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
